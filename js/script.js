ef8bae25c81d7ac9c082ccdf7a1d03accc7188e3
petersutter.pk@gmail.com <petersutter.pk@gmail.com>
2023-04-02 20:28:54 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -3,14 +3,32 @@ $(document).ready(function(){
    $('#menu').click(function(){
     $(this).toggleClass('fa-times');
     $('header').toggleClass('toggle');
+    
    });
 
    $(widow).on('scroll load',function(){
+
     $('#menu').removeClass('fa-times');
     $('header').removeClass('toggle'); 
-   })
+
+   });
   
- 
+    //smooth scrolling
+
+    $('a[href*="#"]').on('click',function(e){
+
+     e.preventDefault();
+
+     $('html, body').animate({
+
+       scrollTop : $($(this).attr('href')).offset().top,
+
+     },     
+     500,
+     'linear'     
+     );
+
+    });
 
 });
 
